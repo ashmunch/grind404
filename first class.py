f7ff266f8a822701c7ab15c5c46419946009354b
unknown <dj530499@gmail.com>
2022-09-28 13:04:01 +0100

    update

---
+++ first class.py
@@ -0,0 +1,35 @@
+from email.quoprimime import body_check
+
+
+
+print("hello world")
+print(type("6.6"))
+
+my_num=35
+print(my_num )
+My_book=14*30
+ruler=2
+print(My_book/ruler)                                                                                              
+
+a=4
+b=10
+x=4*b-2*a
+print(x)
+my_num=2+4+9+7+2+8
+total=6
+output=my_num/total
+print(output)
+
+#given tha t a=4, c=2 and b=7,solve for 
+
+
+a=4
+b=7
+c=2
+numerator=(-b)+(4*a*c)**0.5
+denominator=2*a
+x=numerator/denominator
+print(x)
+
+renegade=65
+print(renegade)
